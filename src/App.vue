4a6b302d37c6ceaa8fe4295b42f940e33234a9d8
Sosna123 <olek.arez@gmail.com>
2024-01-13 12:04:25 +0100

    forgot to do the first commit, logic works but not completly

--- src/App.vue
+++ src/App.vue
@@ -1,27 +1,102 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js + TypeScript App"/>
+    <div class="calculator">
+        <p>{{ value }} {{ operator }} {{ prevValue }}</p>
+        <div class="numbers">
+            <button @click="changeValue(1)">1</button>
+            <button @click="changeValue(2)">2</button>
+            <button @click="changeValue(3)">3</button>
+            <button @click="changeValue(4)">4</button>
+            <button @click="changeValue(5)">5</button>
+            <button @click="changeValue(6)">6</button>
+            <button @click="changeValue(7)">7</button>
+            <button @click="changeValue(8)">8</button>
+            <button @click="changeValue(9)">9</button>
+            <button @click="changeValue(0)">0</button>
+        </div>
+        <div class="operators">
+            <button @click="setOperator('+')">+</button>
+            <button @click="setOperator('-')">-</button>
+            <button @click="setOperator('*')">*</button>
+            <button @click="setOperator('/')">/</button>
+
+            <button @click="count()">=</button>
+            <button @click="clear()">ac</button>
+        </div>
+    </div>
 </template>
 
 <script lang="ts">
-import { defineComponent } from 'vue';
-import HelloWorld from './components/HelloWorld.vue';
-
+import { defineComponent, ref } from 'vue';
 export default defineComponent({
-  name: 'App',
-  components: {
-    HelloWorld
-  }
-});
+    setup(){
+        //* variables
+        const value = ref<number>(0)
+        const prevValue = ref<number>(0)
+        const isPrevValue = ref<boolean>(false)
+        const operator = ref<string>("")
+
+        //* functions
+        function changeValue(add: number){
+            value.value = (value.value * 10) + add
+        }
+
+        function setOperator(operatorButton: string){
+            operator.value = operatorButton
+            if(isPrevValue){
+                count()
+            }
+            prevValue.value = value.value
+            value.value = 0
+            isPrevValue.value = true
+        }
+
+        function count(){
+            if(operator.value == "+"){
+                value.value = prevValue.value + value.value
+                prevValue.value = 0
+                isPrevValue.value = false
+                operator.value = ''
+            }else if(operator.value == "-"){
+                value.value = prevValue.value - value.value
+                prevValue.value = 0
+                isPrevValue.value = false
+                operator.value = ''
+            }else if(operator.value == "*"){
+                value.value = prevValue.value * value.value
+                prevValue.value = 0
+                isPrevValue.value = false
+                operator.value = ''
+            }else if(operator.value == "/"){
+                value.value = prevValue.value / value.value
+                prevValue.value = 0
+                isPrevValue.value = false
+                operator.value = ''
+            }
+        }
+
+        function clear(){
+            value.value = 0
+            prevValue.value = 0
+            isPrevValue.value = false
+            operator.value = ''
+        }
+
+        return{
+            // vars
+            value, prevValue, operator,
+            // functions
+            changeValue, setOperator, count, clear
+        }
+    }
+})
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+p{
+    font-size: 32px;
+}
+
+button{
+    padding: 15px;
 }
 </style>
